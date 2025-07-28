b30de49a0bf7160d1e62c6d0f8ead0d5b39c6079
ege <egeecevit@gmail.com>
2025-07-28 18:23:59 +0300

    FeedbackControllerBase updated

--- romer_custom_controller/include/romer_custom_controller/FeedbackController/FeedbackControllerBase.hpp
+++ romer_custom_controller/include/romer_custom_controller/FeedbackController/FeedbackControllerBase.hpp
@@ -7,7 +7,7 @@
  */
 #pragma once
 
-#include "ros_custom_controller/ControllerBase.hpp"
+#include "romer_custom_controller/ControllerBase.hpp"
 
 #include <ctime>
 
@@ -17,8 +17,8 @@ template<typename Robot>
 class FeedbackControllerBase : public ControllerBase<Robot>
 {
  public:
-  FeedbackControllerBase(ros::NodeHandle* nodeHandle, Robot& robot)
-      : ControllerBase<Robot>(nodeHandle,robot),
+  FeedbackControllerBase(const std::string& node_name, Robot& robot)
+      : ControllerBase<Robot>(node_name,robot),
         time_start_(0.0),
         time_stop_(0.0),
         time_start_ros_(0.0),
@@ -26,39 +26,25 @@ class FeedbackControllerBase : public ControllerBase<Robot>
 
   {
     time_start_ = clock();
-    time_start_ros_ = ros::Time::now().toSec();
-  }
-  ;
+    time_start_ros_ = this->robot_.getNode()->now().seconds();
+  };
 
-  virtual ~FeedbackControllerBase()
-  {
-  }
-  ;
+  virtual ~FeedbackControllerBase() = default;
 
   virtual void create() override
   {
     ControllerBase<Robot>::create();
     x_err_ = Eigen::VectorXd::Zero(this->robot_.getState().size());
    //CONFIRM("create : [Feedback_Controller_Base]");
-  }
-  ;
+  };
 
   virtual void advance(double dt) override
   {
     this->dt_ = dt;
     calculateError();
     calculateInput();
-    //time_stop_ = clock();
-    //time_stop_ros_ = ros::Time::now().toSec();
-    //std::cout << "dt : " <<this->dt_<<" sec" << std::endl;
-    //std::cout << "Time: " << (time_stop_-time_start_)/double(CLOCKS_PER_SEC) << " sec "<< std::endl;
-    //std::cout << "Rime: " << time_stop_ros_-time_start_ros_<<" sec" <<std::endl;
-    //std::cout << "_____________" ;
 
-    //time_start_ = clock();
-    //time_start_ros_ = ros::Time::now().toSec();
-
-  }
+  };
 
  protected:
 
@@ -73,6 +59,5 @@ class FeedbackControllerBase : public ControllerBase<Robot>
   double time_start_ros_;
   double time_stop_ros_;
 
-}
-;
-}
+};
+} // namespace controller
