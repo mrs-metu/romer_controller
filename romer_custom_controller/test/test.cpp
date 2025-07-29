eedf010504784c36a11b3a7c04c1ad0e6737440b
ege <egeecevit@gmail.com>
2025-07-29 19:45:34 +0300

    test.cpp updated

--- romer_custom_controller/test/test.cpp
+++ romer_custom_controller/test/test.cpp
@@ -1,33 +1,30 @@
-#include "ros/ros.h"
+#include <rclcpp/rclcpp.hpp>
 #include <iostream>
-#include "ros_custom_controller/ModelPredictiveController/ModelPredictiveControllerBase.hpp"
-#include "robot_container/RobotModuleContainerBase.hpp"
-#include "robot_container/RobotContainerBase.hpp"
+#include <memory>
+#include <romer_custom_controller/ModelPredictiveController/ModelPredictiveControllerBase.hpp>
+#include <robot_container/RobotModuleContainerBase.hpp>
+#include <robot_container/RobotContainerBase.hpp>
 
 namespace robot {
   class TestRobotContainer : public RobotModuleContainerBase
   {
    public:
-    TestRobotContainer(ros::NodeHandle* nodeHandle,RobotContainerBase& robot):
-      RobotModuleContainerBase(nodeHandle,robot){};
-    virtual ~TestRobotContainer(){};
-    virtual void publish(){};
+    TestRobotContainer(const std::string& node_name) :
+      RobotModuleContainerBase(node_name) {}
+    virtual ~TestRobotContainer() = default;
+    virtual void publish() {}
   };
 }
 
-void testMPC(ros::NodeHandle* nh)
+void testMPC()
 {
-  std::unique_ptr<robot::TestRobotContainer> robot_ ;
-  std::unique_ptr<robot::RobotContainerBase> robotParent_;
-  robotParent_.reset( new robot::RobotContainerBase(nh));
-  robot_.reset(new robot::TestRobotContainer(nh,*robotParent_));
-
+  auto robot_ = std::make_unique<robot::TestRobotContainer>("test_robot");
+  
   robot_->setN(1);
   robot_->setM(1);
 
-  std::unique_ptr<controller::ModelPredictiveControllerBase<robot::TestRobotContainer>> controller_;
-  controller_.reset(new controller::ModelPredictiveControllerBase<robot::TestRobotContainer>(nh,*robot_));
-
+  auto controller_ = std::make_unique<controller::ModelPredictiveControllerBase<robot::TestRobotContainer>>(
+    "test_controller", *robot_);
 
   controller_->create();
 
@@ -42,6 +39,7 @@ void testMPC(ros::NodeHandle* nh)
   Eigen::VectorXd bu = Eigen::VectorXd(2);
   Eigen::MatrixXd Af = Eigen::MatrixXd(2, 1);
   Eigen::VectorXd bf = Eigen::VectorXd(2);
+  
   P << 10;
   Q << 100;
   R << 1000;
@@ -53,6 +51,7 @@ void testMPC(ros::NodeHandle* nh)
   bu << 13, 13;
   Af << 17, 17;
   bf << 19, 19;
+  
   std::cerr << "A\n" << A << std::endl;
   std::cerr << "B\n" << B << std::endl;
   std::cerr << "Ax\n" << Ax << std::endl;
@@ -61,6 +60,7 @@ void testMPC(ros::NodeHandle* nh)
   std::cerr << "bu\n" << bu << std::endl;
   std::cerr << "Af\n" << Af << std::endl;
   std::cerr << "bf\n" << bf << std::endl;
+  
   controller_->setHorizonLength(5);
   controller_->setCostMatrices(P, Q, R);
 
@@ -84,8 +84,8 @@ void testMPC(ros::NodeHandle* nh)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "test");
-  ros::NodeHandle* nh = new ros::NodeHandle("~");
-  testMPC(nh);
+  rclcpp::init(argc, argv);
+  testMPC();
+  rclcpp::shutdown();
   return 0;
 }
